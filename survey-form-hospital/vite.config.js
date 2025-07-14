f18241fe7792aa17cf23fcdb0833a4c6adaa88ac
Dave-Abhishek <daveabhishek2004@gmail.com>
2025-07-14 22:20:44 +0530

    minor fix

--- survey-form-hospital/vite.config.js
+++ survey-form-hospital/vite.config.js
@@ -8,5 +8,5 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
-  base: process.env.VITE_BASE_PATH || "survey-from-hospital"
+  base: process.env.VITE_BASE_PATH || "/survey-from-hospital",
 })
